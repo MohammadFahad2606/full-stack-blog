5adbbaf60debe4f14d9085c38a1d3bcfd53f76c0
MohammadFahad2606 <design4youonline@gmail.com>
2025-01-14 13:40:11 +0500

    db conected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,17 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
+const mongoUri = process.env.MONGO_URI;
+
 const connectDB = async () => {
-    console.log("awra");
-    // try {
-    //     await mongoose.connect(process.env.MONGO_URI, {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //     });
-    //     console.log('MongoDB Connected...');
-    // } catch (err) {
-    //     console.error(err.message);
-    //     process.exit(1);
-    // }
+  console.log(mongoUri);
+  try {
+      await mongoose.connect(mongoUri);
+      console.log('MongoDB Connected...');
+  } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+  }
 };
 
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,27 @@
 import express from 'express';
   import dotenv from 'dotenv';
-import connectDB from './config/db';
+import connectDB from './config/db.js';
 
 // import authRoutes from './routes/authRoutes.js';
-// import userRoutes from './routes/userRoutes.js';
-// import blogRoutes from './routes/blogRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import blogRoutes from './routes/blogRoutes.js';
 
 dotenv.config();
-connectDB();
+
 
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 // Routes
 // app.use('/api/auth', authRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/blogs', blogRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/blogs', blogRoutes);
+const PORT = process.env.PORT || 8000
+app.listen(PORT,()=>{
+  console.log(`Server running on port ${PORT}`) 
+})
+connectDB();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
